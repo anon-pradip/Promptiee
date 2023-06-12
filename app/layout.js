faeebf7937a3e756ff470acd7b620246de92dbc1
anon-pradip <pradipsubedi360@gmail.com>
2023-06-12 18:31:58 +0545

    make mobile responsive navigation

--- app/layout.js
+++ app/layout.js
@@ -12,9 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={inter.className + " bg-slate-100 px-2 container mx-auto"}
-      >
+      <body className={inter.className + " bg-slate-100 px-2"}>
         <Nav />
         {children}
       </body>
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,156 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+
+import { signOut, signIn, getProviders, useSession } from "next-auth/react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+
+  const isUser = true;
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+    setProviders();
+  }, []);
+
+  return (
+    <nav className=" mt-4 flex justify-between items-center">
+      <Link href={"/"} className=" flex gap-2  items-center">
+        <Image
+          src={"/assets/images/logo.svg"}
+          alt="Main logo"
+          height={30}
+          width={30}
+        />
+        <p className=" hidden md:block text-black font-semibold text-xl">
+          Promptiee
+        </p>
+      </Link>
+
+      <div className="hidden sm:flex">
+        {isUser ? (
+          <div className=" flex justify-center gap-2">
+            <Link
+              href={"/create-prompt"}
+              className=" bg-black border border-black text-white hover:bg-transparent hover:text-black rounded-2xl px-3 py-2"
+            >
+              Create Post
+            </Link>
+
+            <button
+              className="
+            bg-white 
+            border
+             border-black
+            text-black 
+            hover:bg-black 
+            hover:text-white rounded-2xl px-3 py-2
+            "
+              onClick={signOut}
+            >
+              Sign Out
+            </button>
+
+            <Link href={"/profile"}>
+              <Image
+                src={"/assets/images/logo.svg"}
+                height={37}
+                width={37}
+                alt="Prfile-Pic"
+                className="rounded-full"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  onClick={() => signIn(provider.id)}
+                  className=" bg-black border border-black text-white hover:bg-transparent hover:text-black rounded-2xl px-3 py-2"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/* mobile navigation */}
+
+      <div className="flex relative sm:hidden">
+        {isUser ? (
+          <div className=" flex justify-center gap-2">
+            <Image
+              src={"/assets/images/logo.svg"}
+              height={37}
+              width={37}
+              alt="Prfile-Pic"
+              className="rounded-full"
+              onClick={() => {
+                setToggleDropdown((prev) => !prev);
+              }}
+            />
+            {toggleDropdown && (
+              <div className=" absolute top-10 right-3 bg-white whitespace-nowrap rounded-md flex flex-col justify-center items-center border border-slate-400 space-y-3">
+                <Link
+                  href={"/profile"}
+                  className="bg-slate-50 hover:bg-slate-200 px-2 rounded-t-md text-center w-full "
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href={"/create-prompt"}
+                  className="
+                  bg-slate-50 hover:bg-slate-200 px-2 rounded-t-md text-center 
+                "
+                >
+                  Create Post
+                </Link>
+                <button
+                  className="
+            bg-white 
+            border-t
+             border-black
+            text-black 
+            hover:bg-black 
+            hover:text-white px-3 w-full
+            "
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  onClick={() => signIn(provider.id)}
+                  className=" bg-black border border-black text-white hover:bg-transparent hover:text-black rounded-2xl px-3 py-2"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
