d9bec6c590ae3a7d3f45b005c5106f09306717fb
anon-pradip <pradipsubedi360@gmail.com>
2023-06-13 19:11:41 +0545

    make google, mongodb uri connection

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,22 @@
+import { connectToDB } from "@/utils/database";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  async session({ session }) {},
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
+});
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import Nav from "@/components/Nav";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Provider from "@/components/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,8 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className + " bg-slate-100 px-2"}>
-        <Nav />
-        {children}
+        <Provider>
+          <Nav />
+          {children}
+        </Provider>
       </body>
     </html>
   );
